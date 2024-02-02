56c9f3460c6b1f12b82dbbd171e87aae3fa13113
Emre Şener <emresener1608@gmail.com>
2024-02-02 23:06:38 +0300

    Add project files.

---
+++ MeyawoPortfolio/Controllers/AbaoutController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MeyawoPortfolio.Models;
+
+
+namespace MeyawoPortfolio.Controllers
+{
+    public class AbaoutController : Controller
+    {
+     DbMyPortfolioEntities db = new DbMyPortfolioEntities();
+        public ActionResult Index()
+        {
+            var values = db.TblAbaout.ToList();
+            return View(values);
+            
+        }
+    }
+}
---
+++ MeyawoPortfolio/Controllers/StatisticController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MeyawoPortfolio.Models;
+    
+    
+namespace MeyawoPortfolio.Controllers
+{
+    public class StatisticController : Controller
+    {
+        DbMyPortfolioEntities db = new DbMyPortfolioEntities();
+        public ActionResult Index()
+        {
+            ViewBag.categoryCount = db.TblCategory.Count();
+            ViewBag.projectCount = db.TblProject.Count();
+            ViewBag.messageCount =db.TblCantact.Count();
+            ViewBag.flutterProjectCount =db.TblProject.Where(x=>x.ProjectCategory ==1).Count();
+            ViewBag.isReadMessageCount = db.TblCantact.Where(x=> x.Isread == false).Count();
+            ViewBag.lastProjectName = db.LastProjectName().FirstOrDefault();
+
+            return View();
+        }
+    }
+}
